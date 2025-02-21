0750b1e175d3291248732909bcfed8b63021e1e8
Arpit1815Coder <arpityadav1518@gmail.com>
2025-02-21 16:32:42 +0530

    commmit

--- Voting-System/src/components/Sidebar.jsx
+++ Voting-System/src/components/Sidebar.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaUser, FaVoteYea, FaPoll, FaSignOutAlt, FaUserPlus, FaListAlt, FaKey, FaExchangeAlt } from "react-icons/fa";
 
 const Sidebar = ({ userType }) => {
   const userLinks = [
-    { name: "Guide", path: "/user-dashboard/guide" },
-    { name: "Voter Registration", path: "/user-dashboard/register" },
-    { name: "Voting Area", path: "/user-dashboard/vote" },
-    { name: "Results", path: "/user-dashboard/results" },
-    { name: "Logout", path: "/" },
+    { name: "Guide", path: "/user-dashboard/guide", icon: <FaUser /> },
+    { name: "Voter Registration", path: "/user-dashboard/register", icon: <FaUserPlus /> },
+    { name: "Voting Area", path: "/user-dashboard/vote", icon: <FaVoteYea /> },
+    { name: "Results", path: "/user-dashboard/results", icon: <FaPoll /> },
+    { name: "Logout", path: "/", icon: <FaSignOutAlt /> },
   ];
 
   const adminLinks = [
-    { name: "Add Candidate", path: "/admin-dashboard/add-candidate" },
-    { name: "Candidate Details", path: "/admin-dashboard/candidates" },
-    { name: "Authentication", path: "/admin-dashboard/register" },
-    { name: "Change Phase", path: "/admin-dashboard/change-phase" },
-    { name: "Logout", path: "/" },
+    { name: "Add Candidate", path: "/admin-dashboard/add-candidate", icon: <FaUserPlus /> },
+    { name: "Candidate Details", path: "/admin-dashboard/candidates", icon: <FaListAlt /> },
+    { name: "Authentication", path: "/admin-dashboard/register", icon: <FaKey /> },
+    { name: "Change Phase", path: "/admin-dashboard/change-phase", icon: <FaExchangeAlt /> },
+    { name: "Logout", path: "/", icon: <FaSignOutAlt /> },
   ];
 
   const links = userType === "user" ? userLinks : adminLinks;
@@ -33,14 +34,14 @@ const Sidebar = ({ userType }) => {
           <li key={link.name}>
             <Link
               to={link.path}
-              className={`block px-4 py-3 font-mono font-bold rounded-lg transition-all duration-300 hover:bg-gray-700 
+              className={`flex items-center px-2 py-3 font-mono font-bold rounded-lg transition-all duration-300 hover:bg-gray-700 
                 ${
                   link.name === "Logout"
                     ? "text-red-500 hover:bg-red-800 hover:text-white"
                     : "text-white/70 hover:text-gray-200"
                 }`}
             >
-              {link.name}
+              <span className="mr-3 text-lg">{link.icon}</span> {link.name}
             </Link>
           </li>
         ))}
